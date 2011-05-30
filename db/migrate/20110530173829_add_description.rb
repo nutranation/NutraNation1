45b5cc25cfa5fcbb9cec5644b4d87e310c0bf343
Spencer Kline <spencer.kline@gmail.com>
2011-05-30 13:47:11 -0400

    deletes unneccesary migration

--- db/migrate/20110530173829_add_description.rb
+++
@@ -1,9 +0,0 @@
-class AddDescription < ActiveRecord::Migration
-  def self.up
-    add_column :users, :description, :text
-  end
-
-  def self.down
-     remove_column :users, :description
-  end
-end
