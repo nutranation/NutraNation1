4975fa4596e41148d116c2d2729d1a8d1aced9d3
Spencer Kline <spencer.kline@gmail.com>
2011-06-18 20:46:42 -0400

    final following

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,26 +2,37 @@ class RelationshipsController < ApplicationController
   before_filter :authenticate
 
     def create 
-      @user = User.find(params[:relationship][:followed_id])
-      current_user.follow!(@user, params[:relationship][:item_type])
-      respond_to do |format|
-        format.html { redirect_to @user }
-        if params[:relationship][:item_type] == 'User'
+      if params[:relationship][:item_type] == 'User'
+        @user = User.find(params[:relationship][:followed_id])
+        current_user.follow!(@user, params[:relationship][:item_type])
+        respond_to do |format|
+          format.html { redirect_to @user }
           format.js { render :action => "create_user" }
-        else
+        end
+      else
+        @tag = Tag.find(params[:relationship][:followed_id])
+        current_user.follow!(@tag, params[:relationship][:item_type])
+        respond_to do |format|
+          format.html { redirect_to @tag }
           format.js { render :action => "create_tag" }
         end
       end
     end
 
     def destroy
-      @user = Relationship.find(params[:id]).followed
-      current_user.unfollow!(params[:id])
-      respond_to do |format|
-        format.html { redirect_to @user }
-        if params[:item_type] == 'User'
+      if params[:item_type] == 'User'
+        @user = Relationship.find(params[:id]).followed
+        current_user.unfollow!(params[:id])
+        respond_to do |format|
+          format.html { redirect_to @user }
           format.js { render :action => "destroy_user" }
-        else
+        end
+      else
+        r = Relationship.find(params[:id]).followed_id
+        @tag = Tag.find r
+        current_user.unfollow!(params[:id])
+        respond_to do |format|
+          format.html { redirect_to @tag }
           format.js { render :action => "destroy_tag" }
         end
       end
