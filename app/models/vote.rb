942b37b2ca8794def42dd0d36ea951399a573643
Spencer Kline <spencer.kline@gmail.com>
2011-06-27 21:40:57 -0400

    adds vote?

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,4 +6,8 @@ class Vote < ActiveRecord::Base
     self.where("content_type = ? AND content_id = ?", content_type, content_id).sum("value")
   end
   
+  def self.voted?(content_type, content_id, user_id)
+    self.where("content_type = ? AND content_id = ?, user_id = ?", content_type, content_id, user_id).first
+  end
+  
 end
