91fe6897a2ecddd4e87b780fea67afc98eb9d726
Spencer Kline <spencer.kline@gmail.com>
2011-07-03 10:40:50 -0400

    adds email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,13 @@ NutraNation::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'spencer.kline',
+    :password             => 'skpass2334',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
