0a87afa3b41db16525d292acecb440e60cc8257d
Spencer Kline <spencer.kline@gmail.com>
2011-07-03 10:06:35 -0400

    adds gmail mailer method

---
+++ config/environments/env.rb
@@ -0,0 +1,9 @@
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'baci.lindsaar.net',
+  :user_name            => 'spencer.kline',
+  :password             => 'skpass2334',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
