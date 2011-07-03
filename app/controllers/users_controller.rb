d8eed072a1ec422f18f4397f2e3f6ec238912182
Spencer Kline <spencer.kline@gmail.com>
2011-07-02 20:34:54 -0400

    adds stuff for emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       sign_in @user
       redirect_to root_path, :flash => { :success => "Welcome to the Sample App!" }
     else
