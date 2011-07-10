90b370f1164fabc1712ecffbf8db26fa800c5e15
Spencer Kline <spencer.kline@gmail.com>
2011-07-09 21:51:42 -0400

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@
 
 class Post < ActiveRecord::Base
   include PgSearch
-  pg_search_scope :search_by_content, :against => [:title, :content], :using => :tsearch 
+  pg_search_scope :search_by_content, :against => [:title, :content], :using => {:tsearch => {:prefix => true}}
         
   
   attr_accessible :title, :content, :picture
