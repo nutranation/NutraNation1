b09c19cf80fa3fb6e639cbf756f36e3df5420aea
Spencer Kline <spencer.kline@gmail.com>
2011-08-17 19:51:13 -0400

    bug fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -201,6 +201,6 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
-  config.omniauth :facebook, "134934403267879", "ffbef8a5e8953028b4b5d97fccde0bc3"
+  config.omniauth :facebook, "134934403267879", "ffbef8a5e8953028b4b5d97fccde0bc3",
         {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
