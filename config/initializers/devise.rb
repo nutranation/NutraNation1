e181b5096f5bcbb58f6d5399244fe24959da465c
Spencer Kline <spencer.kline@gmail.com>
2011-08-17 20:25:16 -0400

    trying to grab name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -202,5 +202,5 @@ Devise.setup do |config|
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
   config.omniauth :facebook, "134934403267879", "ffbef8a5e8953028b4b5d97fccde0bc3",
-        {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+        {:scope => 'email, name, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
