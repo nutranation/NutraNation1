361e89b7f48863c093b9b3739017e5b61911bdd5
Spencer Kline <spencer.kline@gmail.com>
2011-11-06 12:48:35 -0500

    minor changes

--- public/javascripts/nutranation.js
+++ public/javascripts/nutranation.js
@@ -12,3 +12,18 @@ $('div.close_x').click(function () {
 	$(".about_text").hide();
 	$(".contact_text").hide();
 });
+
+
+$('a.fb_connect_button').click(function () { 
+	$.ajax({
+	  url: 'http://www.nutranation.org/users/auth/facebook',
+	  success: function(data) {
+	  	$(".thankyou_box").show();
+	  },
+		error:function (xhr, ajaxOptions, thrownError){
+	                    alert(xhr.statusText);
+	                    alert(thrownError);
+	                }
+	});
+	
+})
